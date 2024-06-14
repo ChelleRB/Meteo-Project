1fc67d6a5d92ab098f700f879015fe5f53f7f2d4
Rochelle Anderson <Rochellebrandy@icloud.com>
2024-06-14 23:32:15 +0100

    CORRECTED api bug

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
+
 function searchCity(city) {
   let apiKey = "54a0b1t705328c1ff5aabef7485od2a3";
-  let apiurl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric";
-  axios.get(apiurl).then(refreshWeather);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 //made api call and update interface above
 //apiurl will search for a city
@@ -18,11 +18,10 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
-//h1 name will change, no api yet
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
